9ef65c50f563b3af3f3413ba3db9e781b8b1f2ea
LividLuc1d <93408137+LividLuc1d@users.noreply.github.com>
2024-04-23 10:23:06 +0200

    computeAddTest

--- src/test/java/JavaFXAppTest.java
+++ src/test/java/JavaFXAppTest.java
@@ -1,9 +1,16 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+
 class JavaFXAppTest {
+
     @Test
-    void aDummyTest() {
-        assertEquals(0,0);
+    void computeAddTest() {
+        JavaFXApp java = new JavaFXApp();
+
+        int result = java.computeAdd(4, 4);
+        assertEquals(8,result);
     }
+
+
 }
