ff85def1485b2aaea10fdcedb288f9c5f1a0d6ae
Kohyar1 <162112343+Kohyar1@users.noreply.github.com>
2024-04-23 10:29:12 +0200

    divide + multiply test

--- src/test/java/JavaFXAppTest.java
+++ src/test/java/JavaFXAppTest.java
@@ -3,7 +3,21 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class JavaFXAppTest {
     @Test
-    void aDummyTest() {
-        assertEquals(0,0);
+    void computeAddTest() {
+        JavaFXApp java = new JavaFXApp();
+        int result = java.computeAdd(4, 4);
+        assertEquals(8,result);
+    }
+    @Test
+    void computeMultiplyTest() {
+        JavaFXApp java = new JavaFXApp();
+        int result = java.computeMultiply(16, 16);
+        assertEquals(256,result);
+    }
+    @Test
+    void computeDivideTest() {
+        JavaFXApp java = new JavaFXApp();
+        int result = java.computeDivide(16, 2);
+        assertEquals(8,result);
     }
 }
